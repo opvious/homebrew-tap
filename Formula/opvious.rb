2a841e2f43f9fb65429c2c1920b0ee4304d099c0
Matthieu Monsch <matthieu.monsch@gmail.com>
2024-11-28 10:24:52 -0800

    feat: add SHA256

--- Formula/opvious.rb
+++ Formula/opvious.rb
@@ -4,6 +4,7 @@ class Opvious < Formula
   license "Apache-2.0"
   version "0.1.0"
   url "https://registry.npmjs.org/opvious-cli/-/opvious-cli-0.12.1.tgz"
+  sha256 "a9dbcac15921502721f2f1b0782602c7f98720123c9d0487aea1cb2cd6a0e61f"
 
   depends_on "node"
 
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Homebrew tap
 
-WIP
+This is the official [Homebrew][] tap for [Opvious][].
+
+```sh
+brew install opvious/tap/opvious # Install
+brew uninstall opvious # Uninstall
+```
+
+[Homebrew]: https://brew.sh
+[Opvious]: https://www.opvious.io
